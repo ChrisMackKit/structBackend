e179428b94c647cfbcd4536f9ff1e425012d0d8d
Christian Mack <pfenning@rocketmail.com>
2019-01-05 14:45:23 +0100

    Funktionen für Berechnen der Koordinaten hinzugefügt ohen Testen

--- src/main/java/edu/kit/tm/cm/backend/domain/model/Coordinates.java
+++ src/main/java/edu/kit/tm/cm/backend/domain/model/Coordinates.java
@@ -3,8 +3,17 @@ package edu.kit.tm.cm.backend.domain.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
+
+/**
+ * Coordinates: (x,y) == (longitude, latitude)
+ * (x,y) = (0,0) --> top left corner
+ * (lon, lat) = (0,0) --> bottom left corner
+ * So X and longitude point in the same direction
+ * Y and latitude point in different directions. Y down and Lat up (at least in the north part of the globe)
+ */
 
 @Entity
 @AllArgsConstructor
@@ -12,5 +21,16 @@ import javax.persistence.Entity;
 public class Coordinates {
 
     @Getter
-    private int[] xy = new int[2];
+    @Setter
+    private double[] xy = new double[2];
+    //Setter for x coordinate
+    public void setX(double x) {
+        this.xy[0] = x;
+    }
+    //Setter for y coordinate
+    public void setY(double y) {
+        this.xy[1] = y;
+    }
+
+
 }
